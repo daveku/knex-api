a5fa7f7c03524fe8b6a577e61d7146516b0a8ffe
David Ku <hola@davidku.dev>
2022-02-15 16:54:51 -0500

    se añade comentario

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -3,8 +3,8 @@ const router = express.Router();
 
 const homeController = require("../controllers/homeController");
 
-router.post("/homes", homeController.createHome);
-router.get("/homes", homeController.getAllHomes);
-router.get("/homes/:id", homeController.getHome);
+router.post("/homes", homeController.createHome); // Create
+router.get("/homes", homeController.getAllHomes); // Read all
+router.get("/homes/:id", homeController.getHome); // Read one
 
 module.exports = router;
